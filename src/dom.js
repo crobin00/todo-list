bf64a63afc61b9f22868f8f9f884aef6cf60831c
chris <christanr00@gmail.com>
2021-07-13 21:20:38 -0400

    added functionality to add new task to div

--- src/dom.js
+++ src/dom.js
@@ -16,6 +16,7 @@ function init() {
 	selectList();
 	newTaskDisplay();
 	closeTaskDisplay();
+	addTaskToDiv();
 }
 
 function createNewList() {
@@ -92,5 +93,37 @@ function closeTaskDisplay() {
 		sidebar.classList.remove("opacity");
 		mainDisplay.classList.remove("opacity");
 		newTaskDisplay.classList.add("hide");
+	});
+}
+
+function addTaskToDiv() {
+	const title = document.querySelector("#new-book-title");
+	const description = document.querySelector("#new-book-description");
+	const dueDate = document.querySelector("#new-book-due-date");
+	const urgent = document.querySelector("#urgent");
+	const normal = document.querySelector("#normal");
+	const doneButton = document.querySelector(".add-task-button");
+
+	doneButton.addEventListener("click", (e) => {
+		const taskDiv = document.querySelector(".task-div");
+		const newTask = document.createElement("div");
+		newTask.innerHTML = `<div class="priority"></div>
+        <i class="fas fa-times-circle"></i>
+        <h5>${title.value}</h5>
+        <p>
+            ${description.value}
+        </p>
+        <i class="fas fa-check-circle"></i>
+        <span
+            >Due date <br />
+            ${dueDate.value}
+        </span>
+        <i class="fas fa-edit"></i>`;
+
+		if (urgent.checked) console.log("urgent");
+		if (normal.checked) console.log("normal");
+
+		newTask.classList.add("task");
+		taskDiv.appendChild(newTask);
 	});
 }
